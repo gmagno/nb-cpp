eb86b02656c693221f6f54cfd74f8d784033a24e
Goncalo <goncalo.magno@gmail.com>
2019-06-01 18:40:33 -0300

    Update example

--- example/nb6order.py
+++ example/nb6order.py
@@ -1,4 +1,6 @@
 
+import time
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,13 +8,28 @@ import numpy as np
 import nb_cpp
 
 def main():
+    crmin = -10.0
+    crmax = 10.0
+    cimin = -5.0
+    cimax = 5.0
+    ratio = (crmax - crmin) / (cimax - cimin)
+
+    # imw = 2048
+    # imw = 4096
+    # imw = 8192
+    imw = 1024
+
+    t = time.time()
     hsv = nb_cpp.compute(
-        imw=128, imh=128,
+        imw=imw, imh=int(imw/ratio),
         coefs=np.array([1, 0, 0, 0, 0, 0, 1], dtype=np.float),
-        crmin=-5.0, crmax=5.0,
-        cimin=-5.0, cimax=5.0,
+        crmin=crmin, crmax=crmax,
+        cimin=cimin, cimax=cimax,
         itmax=30, tol=1e-6
     )
+    e = time.time() - t
+    print('Elapsed time: %.2f' % (e,))
+
     rgb = mpl.colors.hsv_to_rgb(hsv)
     plt.figure('Example: Newton Basins 6th order poly.')
     plt.imshow(rgb)
